ccbbb2bf1b5d561ed9ba27a443423b4f166af6f8
Flamey <flamey@hexa.blue>
2024-03-31 21:24:52 +0200

    tired cd ..!

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -3,6 +3,12 @@ import type { Metadata } from "next"
 export const metadata: Metadata = {
     title: "dibsy",
     description: "A serverless social For Everything You.",
+    authors: {
+        name: "HexaBlue",
+        url: "https://hexa.blue"
+    },
+    keywords: ["social", "p2p", "peer to peer", "serverless", "privacy"]
+    // add OpenGraph and Twitter later
 }
 
 export default function RootLayout({children}: Readonly<{children: React.ReactNode}>) {
